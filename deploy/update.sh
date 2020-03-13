6d143ce0e4b4612dd0f9566e179a8a2574351c19
root <root@docker.vm.devel.cape-it.de>
2020-03-13 16:34:45 +0100

    some enhancements

--- deploy/update.sh
+++ deploy/update.sh
@@ -4,6 +4,6 @@
 source ./environment
 export $(cut -d= -f1 ./environment | egrep '^[A-Z]')
 
-docker-compose -p kix pull
-docker-compose -p kix up -d
-docker compose -p kix restart proxy
+docker-compose -p kix pull 
+docker-compose -p kix up -d $@
+docker-compose -p kix restart proxy
