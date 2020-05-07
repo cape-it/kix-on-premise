5ad13b43fa495070b8068763058947af6efe5117
Tobias Czirfusz <tobias.czirfusz@cape-it.de>
2020-05-07 10:19:41 +0200

    Power Shell Scripts for Windows

---
+++ deploy/update.ps1
@@ -0,0 +1,7 @@
+﻿(Get-Content .\environment) -cmatch '^[A-Z]' | ForEach-Object {
+    Set-Item -path "env:$($_.split('=')[0])" -value $_.split('=')[1] -Force;
+}
+
+docker-compose -p kix pull
+docker-compose -p kix up -d
+docker-compose -p kix restart proxy
